a4092970175fc74e81db0ee982efb41b6397599a
Giuseppe De Marco <giuseppe.demarco@unical.it>
2020-05-01 16:36:31 +0200

    removed try/except with dict.get

--- djangosaml2/views.py
+++ djangosaml2/views.py
@@ -283,9 +283,8 @@ def assertion_consumer_service(request,
     create_unknown_user = create_unknown_user if create_unknown_user is not None else \
                           get_custom_setting('SAML_CREATE_UNKNOWN_USER', True)
     conf = get_config(config_loader_path, request)
-    try:
-        xmlstr = request.POST['SAMLResponse']
-    except KeyError:
+    xmlstr = request.POST.get('SAMLResponse')
+    if not xmlstr:
         logger.warning('Missing "SAMLResponse" parameter in POST data.')
         raise SuspiciousOperation
 
