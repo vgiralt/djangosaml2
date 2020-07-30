251364179aeca54b6d4ed940df36b414194fd994
Giuseppe <giuseppe.demarco@unical.it>
2020-07-30 14:58:43 +0200

    v0.30.0

--- tests/settings.py
+++ tests/settings.py
@@ -26,7 +26,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'testprofiles',
-    
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +45,8 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'djangosaml2.middleware.SamlSessionMiddleware'
 )
 
 ROOT_URLCONF = 'testprofiles.urls'
